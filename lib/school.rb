37c37511973b2ba002360461646817fc3297435d
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-26 11:48:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,15 +8,9 @@ class School
   end
 
   def add_student(student, grade)
-    if @roster.has_key(grade)
-      @roster[grade] << student
-    else
-      @roster[grade] = [student]
-    end
-
-  #  @roster[grade] ||=[student]
-  #  @roster[grade] = []
-  #  @roster[grade] << name
+    roster[grade] ||= []
+    roster[grade] << student
   end
 
 end
+
